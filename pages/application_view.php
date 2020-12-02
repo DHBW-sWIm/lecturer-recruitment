d0d744615c1d187b9dfbe6f94a37f0a8fe5762cd
VanessaStz <v.stutz1@gmail.com>
2020-12-02 16:27:31 +0100

    Sprachanpassung
    Die Sprache wurde in der Übersicht des Bewerberprofils auf Deutsch umgestellt.

--- pages/application_view.php
+++ pages/application_view.php
@@ -1,5 +1,6 @@
-<?php
 
+<?php
+//Bewerberprofil über Anwendung: Recruitment Prozesse
 
 require(dirname(__FILE__, 4) . '/config.php');
 require_login();
@@ -27,24 +28,24 @@ if (has_capability('local/lecrec:manager', $context)) {
     $PAGE->set_heading('Lecturer Recruitment');
     $PAGE->navbar->add('Lecturer Recruitment', new moodle_url('/local/lecrec/index.php'));
     $PAGE->navbar->add('Recruitment Processes', new moodle_url('/local/lecrec/pages/recruitmentprocess.php'));
-    $PAGE->navbar->add('Applications', new moodle_url('/local/lecrec/pages/application_overview.php'));
+    $PAGE->navbar->add('Bewerber', new moodle_url('/local/lecrec/pages/application_overview.php'));
     echo $OUTPUT->header();
-    echo $OUTPUT->heading('Application');
+    echo $OUTPUT->heading('Bewerber');
     $record = $DB->get_record('lr_application', array('id' => $RecordID));
 }
 $icon = '010-mail.svg';
 $status = 'pending';
-if ($record->status_of_application === 'Rejected') {
-    $interviewsent = 'active';
-    $accepted = 'notactive';
+if ($record->status_of_application === 'Abgelehnt') {
+    $interviewsent = 'Aktiv';
+    $accepted = 'Nicht Aktiv';
     $icon = '003-reject-2.svg';
-    $status = 'rejected';
-} elseif ($record->status_of_application === 'Accepted') {
-    $interviewsent = 'active';
-    $accepted = 'active';
-    $status = 'accepted';
-} elseif ($record->status_of_application === 'Invitation sent') {
-    $interviewsent = 'active';
+    $status = 'Abgelehnt';
+} elseif ($record->status_of_application === 'Akzeptiert') {
+    $interviewsent = 'Aktiv';
+    $accepted = 'Aktiv';
+    $status = 'Akzeptiert';
+} elseif ($record->status_of_application === 'Einladung gesendet') {
+    $interviewsent = 'Aktiv';
 
 
 }
@@ -91,28 +92,28 @@ echo html_writer::start_div('');
 echo html_writer::start_div('card');
 echo html_writer::start_div('card-header');
 echo html_writer::start_tag('a', array('class' => 'card-link', 'data-toggle' => "collapse", 'href' => "#personal-info"));
-echo "Personal information";
+echo "Persönliche Informationen";
 echo html_writer::end_tag('a');
 echo html_writer::end_div();
 echo html_writer::start_div('collapse show', array('id' => "personal-info"));
 echo html_writer::start_div('card-body');
 echo html_writer::start_div('row');
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Name and title', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Name und Titel', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->title . ' ' . $record->fname . ' ' . $record->lname);
-echo html_writer::tag('h6', 'Home address', array('class' => "font-weight-bold"));
-echo html_writer::tag('address', $record->private_add_str . '<br>' . $record->private_add_zip . ' ' . $record->private_add_city . '<br> <abbr title="Phone">P: </abbr>' . $record->private_tele . '<br> <abbr title="Mobile">M: </abbr>' . $record->private_mobile . '<br> <abbr title="Fax">F: </abbr>' . $record->private_fax);
+echo html_writer::tag('h6', 'Private Adresse', array('class' => "font-weight-bold"));
+echo html_writer::tag('Addresse', $record->private_add_str . '<br>' . $record->private_add_zip . ' ' . $record->private_add_city . '<br> <abbr title="Phone">P: </abbr>' . $record->private_tele . '<br> <abbr title="Mobile">M: </abbr>' . $record->private_mobile . '<br> <abbr title="Fax">F: </abbr>' . $record->private_fax);
 echo html_writer::end_div();
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Email address', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'E-Mail Adresse', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->private_email);
-echo html_writer::tag('h6', 'Education', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Ausbildung', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->education);
 echo html_writer::end_div();
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Date of birth', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Geburtsdatum', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', (new DateTime($record->date_of_birth))->format('d-m-Y'));
-echo html_writer::tag('h6', 'Place of birth', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Geburtsort', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->place_of_birth);
 echo html_writer::end_div();
 echo html_writer::end_div();
@@ -123,22 +124,22 @@ echo html_writer::start_div('');
 echo html_writer::start_div('card');
 echo html_writer::start_div('card-header');
 echo html_writer::start_tag('a', array('class' => 'card-link', 'data-toggle' => "collapse", 'href' => "#work-info"));
-echo "Work information";
+echo "Informationen zur Arbeitsstelle";
 echo html_writer::end_tag('a');
 echo html_writer::end_div();
 echo html_writer::start_div('collapse show', array('id' => "work-info"));
 echo html_writer::start_div('card-body');
 echo html_writer::start_div('row');
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Company name', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Firmenname', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->company);
-echo html_writer::tag('h6', 'Work address', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Firmenadresse', array('class' => "font-weight-bold"));
 echo html_writer::tag('address', $record->company_add_str . '<br>' . $record->company_add_zip . ' ' . $record->company_add_city . '<br> <abbr title="Phone">P: </abbr>' . $record->company_tele . '<br> <abbr title="Fax">F: </abbr>' . $record->company_fax);
 echo html_writer::end_div();
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Work Email address', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Firmen E-Mail Adresse', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->company_email);
-echo html_writer::tag('h6', 'Job', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Berufsbezeichnung', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->job);
 echo html_writer::end_div();
 echo html_writer::end_div();
@@ -149,22 +150,22 @@ echo html_writer::start_div('');
 echo html_writer::start_div('card');
 echo html_writer::start_div('card-header');
 echo html_writer::start_tag('a', array('class' => 'card-link', 'data-toggle' => "collapse", 'href' => "#interests"));
-echo "Experience and Preferences ";
+echo "Erfahrungen und Präferenzen";
 echo html_writer::end_tag('a');
 echo html_writer::end_div();
 echo html_writer::start_div('collapse show', array('id' => "interests"));
 echo html_writer::start_div('card-body');
 echo html_writer::start_div('row');
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Previous teaching activities', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Vorherige Lehraktivitäten', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->teaching_activities);
 echo html_writer::end_div();
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Job activities', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Berufliche Aufgaben', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->job_activities);
 echo html_writer::end_div();
 echo html_writer::start_div('col-md-4');
-echo html_writer::tag('h6', 'Subjects the applicant is interested in teaching', array('class' => "font-weight-bold"));
+echo html_writer::tag('h6', 'Präferenzierte Lehrfächer', array('class' => "font-weight-bold"));
 echo html_writer::tag('p', $record->subject_of_interest);
 echo html_writer::end_div();
 echo html_writer::end_div();
@@ -174,29 +175,29 @@ echo html_writer::end_div();
 echo html_writer::end_div();
 //TODO connect with camunda to qualify there
 
-if ($record->status_of_application == 'Invitation sent') {
+if ($record->status_of_application == 'Einladung versandt') {
     echo '<br><form action="../assets/PHPFunctions/respond_to_application.php" method="post">
 <div class="row mx-auto">
 <input name="app_id" value="' . $RecordID . '" hidden>
-<input type ="submit" name="approve" class="btn btn-success" value="Approve">
-<input type ="submit" name="qualify" class="btn btn-info" value="New interview">
-<input type ="submit" name="reject" class="btn btn-danger" value="Reject">
+<input type ="submit" name="approve" class="btn btn-success" value="Genehmigen">
+<input type ="submit" name="qualify" class="btn btn-info" value="Neues Gespräch">
+<input type ="submit" name="reject" class="btn btn-danger" value="Ablehnen">
 </div>
 </from>';
-} else if ($record->status_of_application == 'Applied') {
+} else if ($record->status_of_application == 'Beworben') {
     echo '<br><form action="../assets/PHPFunctions/respond_to_application.php" method="post">
 <div class="row mx-auto">
 <input name="app_id" value="' . $RecordID . '" hidden>
-<input type ="submit" name="qualify" class="btn btn-success" value="Qualify to interview">
-<input type ="submit" name="reject" class="btn btn-danger" value="Reject">
+<input type ="submit" name="qualify" class="btn btn-success" value="Zu 1. Gespräch einladen">
+<input type ="submit" name="reject" class="btn btn-danger" value="Ablehnen">
 </div>
 </from>';
-} else if ($record->status_of_application == 'Waiting to interview') {
+} else if ($record->status_of_application == 'Gespräch ausstehend') {
     echo '<br><form action="../assets/PHPFunctions/respond_to_application.php" method="post">
 <div class="row mx-auto">
 <input name="app_id" value="' . $RecordID . '" hidden>
-<input type ="submit" name="qualify" class="btn btn-info" value="New interview">
-<input type ="submit" name="reject" class="btn btn-danger" value="Reject">
+<input type ="submit" name="qualify" class="btn btn-info" value="Neues Gespräch">
+<input type ="submit" name="reject" class="btn btn-danger" value="Ablehnen">
 </div>
 </from>';
 }
